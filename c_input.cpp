389c6fade394fd0ca89b27beee44ee9d4810529e
Zarif130 <jarifh130@gmail.com>
2025-09-02 03:56:45 +0600

    Added.

---
+++ c_input.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    cout<<"so the number is = ";
+    cout<<a;
+}
---
+++ c_output.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"let's DO it bro";
+}
