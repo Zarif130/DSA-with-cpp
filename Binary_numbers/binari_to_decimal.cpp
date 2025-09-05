4987891105028846197025c4c8aa435c984e4fef
Zarif130 <jarifh130@gmail.com>
2025-09-05 12:59:32 +0600

    Added.

--- Binary_numbers/binari_to_decimal.cpp
+++ Binary_numbers/binari_to_decimal.cpp
@@ -1,8 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int bainaryTOdacimal(int baiNum);
+int bainaryTOdacimal(int baiNum){
+int ans=0,pow=1;
+    while (baiNum>0)
+    {
+        int remainder = baiNum%10;
+        ans+=remainder*pow;
+        baiNum/=10;
+        pow*=2;
+    }
+    
+    return ans;
+}
 int main(){
 ios_base::sync_with_stdio(false);
 cin.tie(nullptr);
-    
+    int baiNum;
+    cin>>baiNum;
+    cout<< bainaryTOdacimal(baiNum);
+
     return 0;
 }
+
